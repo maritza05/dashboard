a6148f581a60e3ceffaa2a08abceb8673e5fb1d4
Maritza <maritzaesparza05@gmail.com>
2016-10-12 11:55:13 -0500

    Iteration chart

---
+++ panel/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.get_all_projects, name='projects_list'),
+    url(r'^data/(?P<id>\d+)/$', views.show_charts, name="show_charts"),
+    url(r'^data/milestones/(?P<id>\d+)/$', views.get_milestones_stats, name="show_milestone")
+]
